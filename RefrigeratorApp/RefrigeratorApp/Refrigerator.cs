5d2fb45724cb0bdad5a8839b48709108ff340423
faisal-nstu <faisal.nstu@ymail.com>
2014-07-22 10:03:33 +0530

    Refrigerator app completed

---
+++ RefrigeratorApp/RefrigeratorApp/Refrigerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorApp
+{
+    class Refrigerator
+    {
+        public double capacity;
+        public double currentWeight=0;
+        public double remainingWeight;
+
+        public void AddItems(int noOfItems, double weightPerUnit)
+        {
+            double itetTobeAdded = weightPerUnit*noOfItems;
+            if (currentWeight + itetTobeAdded <= capacity)
+            {
+                currentWeight +=  noOfItems*weightPerUnit;
+                remainingWeight = capacity - currentWeight;
+            }
+            else
+            {
+                throw new Exception("Overload...");
+            }
+            
+        }
+    }
+}
